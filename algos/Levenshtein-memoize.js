08bb7d2758232650500737e8db558fbd348d2acf
Valentin Vakar <valentin.vakar@virtualinstruments.com>
2019-11-19 19:47:00 +0000

    Memoized edit distance with path reconstruction

--- algos/Levenshtein-memoize.js
+++ algos/Levenshtein-memoize.js
@@ -1,32 +1,83 @@
 /**
  * Levenshtein distance using recursion/memoizing
+ *
+ * Strategy:
+ *  1. we only need to memoize based on a and b length rather than a and b because it's always going to be the tail end of each.
+ *  2. we additionally memoize each step taken so we can reconstruct the path
+ *
+ *  Step Legend:
+ *  A - keep a side, remove one b
+ *  B - keep b side, remove one a
+ *  E - a and b equal, remove both
+ *  X - a and b different, remove both
+ *
  */
+function fun() {
+    let memo;
 
-function levenshteinMemoize(a, b) {
-    return editDistanceMemo(a,b);
+    function levenshteinMemoize(a, b) {
+        memo = {};
+        memo[key('', '')] = dist('', 0);
+
+        const d = editDistanceMemo(a, b);
+        const path = reconstructPath(a, b);
+        return  { distance: d.distance, path: path };
+    }
+
+    function dist(step, distance) {
+        return { step: step, distance: distance };
+    }
+
+    function key(a, b) {
+        return a.length + '--' + b.length;
+    }
+
+    function editDistanceMemo(a, b) {
+        const prop = key(a,b);
+        if (memo[prop]) return memo[prop];
+
+        return memo[prop] = editDistanceImpl(a, b);
+    }
+
+    function editDistanceImpl(a, b) {
+        if (a.length === 0) return dist('A'.repeat(b.length), b.length);
+        if (b.length === 0) return dist('B'.repeat(a.length), a.length);
+
+        const ahead = a.charAt(0);
+        const atail = a.substring(1);
+        const bhead = b.charAt(0);
+        const btail = b.substring(1);
+
+        if (ahead === bhead)
+            return dist('E', editDistanceMemo(atail, btail).distance);
+        else {
+            const aa = editDistanceMemo(a, btail);
+            const bb = editDistanceMemo(atail, b);
+            const xx = editDistanceMemo(atail, btail);
+
+            if (aa.distance <= bb.distance && aa.distance <= xx.distance)
+                return dist('A', aa.distance + 1);
+            else if (bb.distance <= xx.distance)
+                return dist('B', bb.distance + 1);
+            else
+                return dist('X', xx.distance + 1);
+        }
+    }
+
+    function reconstructPath(a, b) {
+        const k = key(a,b);
+        const dist = memo[k];
+
+        switch(dist.step) {
+            case 'E':
+            case 'X': return dist.step + reconstructPath(a.substr(1), b.substr(1));
+            case 'B': return dist.step + reconstructPath(a.substr(1), b);
+            case 'A': return dist.step + reconstructPath(a, b.substr(1));
+            default: return dist.step;
+        }
+    }
+
+    return levenshteinMemoize;
 }
 
-const memo = {};
-function editDistanceMemo(a, b) {
-    const prop = a + ';;;;;' + b;
-    if (memo[prop]) return memo[prop];
-
-    const res = editDistanceImpl(a,b);
-    memo[prop] = res;
-    return res;
-}
-
-function editDistanceImpl(a, b) {
-    if (a.length === 0) return b.length;
-    if (b.length === 0) return a.length;
-
-    const ahead = a.charAt(0);
-    const atail = a.substring(1);
-    const bhead = b.charAt(0);
-    const btail = b.substring(1);
-
-    if (ahead === bhead) return levenshteinMemoize(atail, btail);
-    else return 1 + Math.min(editDistanceMemo(a, btail), editDistanceMemo(atail, b), editDistanceMemo(atail, btail));
-}
-
-module.exports = levenshteinMemoize;
+module.exports = fun();
--- test/testEditDistance.js
+++ test/testEditDistance.js
@@ -1,5 +1,5 @@
 const describe = require('mocha').describe;
-const editDistance = require('../algos/Levenshtein-memoize');
+const editDistanceMemo = require('../algos/Levenshtein-memoize');
 const chai = require('chai');
 const assert = chai.assert;
 const expect = chai.expect;
@@ -8,36 +8,48 @@ const should = chai.should;
 
 describe('edit distance behaves', function() {
     it('handles edge cases', function(done) {
-        expect(editDistance('', '')).to.eql(0);
-        expect(editDistance('abc', '')).to.eql(3);
-        expect(editDistance('', 'abc')).to.eql(3);
+        expect(editDistanceMemo('', '').distance).to.eql(0);
+        expect(editDistanceMemo('', '').path).to.eql('');
+        expect(editDistanceMemo('abc', '').distance).to.eql(3);
+        expect(editDistanceMemo('abc', '').path).to.eql('BBB');
+        expect(editDistanceMemo('', 'abc').distance).to.eql(3);
+        expect(editDistanceMemo('', 'abc').path).to.eql('AAA');
         done();
     });
 
     it('handles short words', function(done) {
-        expect(editDistance('hello', 'hello')).to.eql(0);
-        expect(editDistance('hello1', 'hello')).to.eql(1);
-        expect(editDistance('1hello1', 'hello')).to.eql(2);
-        expect(editDistance('1hello', 'hello')).to.eql(1);
+        expect(editDistanceMemo('hello', 'hello').distance).to.eql(0);
+        expect(editDistanceMemo('hello', 'hello').path).to.eql('EEEEE');
+        expect(editDistanceMemo('hello', 'hello1').distance).to.eql(1);
+        expect(editDistanceMemo('hello', 'hello1').path).to.eql('EEEEEA');
+        expect(editDistanceMemo('hello1', 'hello').distance).to.eql(1);
+        expect(editDistanceMemo('hello1', 'hello').path).to.eql('EEEEEB');
+        expect(editDistanceMemo('1hello1', 'hello').distance).to.eql(2);
+        expect(editDistanceMemo('1hello1', 'hello').path).to.eql('BEEEEEB');
+
+        expect(editDistanceMemo('hello', '1hello1').distance).to.eql(2);
+        expect(editDistanceMemo('hello', '1hello1').path).to.eql('AEEEEEA');
+        expect(editDistanceMemo('1hello', 'hello').distance).to.eql(1);
+        expect(editDistanceMemo('1hello', 'hello').path).to.eql('BEEEEE');
         done();
     });
 
     it('handles more complex cases', function(done) {
-        expect(editDistance('distnace', 'distance')).to.eql(2);
-        expect(editDistance('diistance', 'distance')).to.eql(1);
-        expect(editDistance('dstance', 'distance')).to.eql(1);
+        expect(editDistanceMemo('distnace', 'distance').distance).to.eql(2);
+        expect(editDistanceMemo('diistance', 'distance').distance).to.eql(1);
+        expect(editDistanceMemo('dstance', 'distance').distance).to.eql(1);
         done();
     });
 
     it('handles very long cases', function(done) {
-        expect(editDistance('completely involved in doing something, especially so that you do not notice things happening around you. deep in conversation: Her husband seemed deep in conversation with Mrs Beeley. deep in thought: He was deep in thought, oblivious to all the noise around him.',
-            'completely involved in doing something, especially so that you do not notice things happening around you. deep in conversation: Her husband seemed deep in conversation with Mrs Beeley. deep in thought: He was deep in thought, oblivious to all the noise around him.')).to.eql(0);
+        expect(editDistanceMemo('completely involved in doing something, especially so that you do not notice things happening around you. deep in conversation: Her husband seemed deep in conversation with Mrs Beeley. deep in thought: He was deep in thought, oblivious to all the noise around him.',
+            'completely involved in doing something, especially so that you do not notice things happening around you. deep in conversation: Her husband seemed deep in conversation with Mrs Beeley. deep in thought: He was deep in thought, oblivious to all the noise around him.').distance).to.eql(0);
 
-        expect(editDistance('completely involved in doing something, especially so that you do not notice things happening around you. deep in conversation: Her husband seemed deep in conversation with Mrs Beeley. deep in thought: He was deep in thought, oblivious to all the noise around him.',
-            'completely involved in doing hing, especially so tat you do nt notice things happning around yu. deep in conversaion: Her husband seemed deep in conversation with Mrs Beeley. deep in thought: He was deep in thought, oblivious to all the noise around him.')).to.eql(10);
+        expect(editDistanceMemo('completely involved in doing something, especially so that you do not notice things happening around you. deep in conversation: Her husband seemed deep in conversation with Mrs Beeley. deep in thought: He was deep in thought, oblivious to all the noise around him.',
+            'completely involved in doing hing, especially so tat you do nt notice things happning around yu. deep in conversaion: Her husband seemed deep in conversation with Mrs Beeley. deep in thought: He was deep in thought, oblivious to all the noise around him.').distance).to.eql(10);
 
-        expect(editDistance('completely involved in doing something, especially so that you do not notice things happening around you. deep in conversation: Her husband seemed deep in conversation with Mrs Beeley. deep in thought: He was deep in thought, oblivious to all the noise around him.',
-        'The world as we have created it is a process of our thinking. ...“If I were a tree, I would have no reason to love a human.” ...“Once someones hurt you, its harder to relax around them, harder to think of them as safe to love. ...')).to.eql(199);
+        expect(editDistanceMemo('completely involved in doing something, especially so that you do not notice things happening around you. deep in conversation: Her husband seemed deep in conversation with Mrs Beeley. deep in thought: He was deep in thought, oblivious to all the noise around him.',
+        'The world as we have created it is a process of our thinking. ...“If I were a tree, I would have no reason to love a human.” ...“Once someones hurt you, its harder to relax around them, harder to think of them as safe to love. ...').distance).to.eql(199);
 
         done();
     });
