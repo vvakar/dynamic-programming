ce174a2373b4978b5dea9d33dbb1b4658d42c725
Valentin Vakar <valentin.vakar@virtualinstruments.com>
2020-11-11 20:50:38 +0000

    Update gradient descent cost function

--- algos/GradientDescent.ts
+++ algos/GradientDescent.ts
@@ -16,6 +16,10 @@ export default function gradientDescent(xs: number[], ys: number[], STEP: number
         return rss;
     }
 
+    function euclideanLength(w0Slope: number, w1Slope: number) {
+        return Math.sqrt(Math.pow(w0Slope, 2) + Math.pow(w1Slope, 2))
+    }
+
     function w0_slope(w0: number, w1: number) {
         let sum = 0;
         for (let i = 0; i < xs.length; ++i) {
@@ -39,7 +43,7 @@ export default function gradientDescent(xs: number[], ys: number[], STEP: number
     let w1 = 0;
     let prevRSS, rss = 9999999999;
     let iterations = 0;
-    let costFunction = computeRSS;
+    var cost = tolerance * 1000;
 
     do {
         console.log("Trying with w0 = " + w0 + ", w1 = " + w1);
@@ -51,10 +55,16 @@ export default function gradientDescent(xs: number[], ys: number[], STEP: number
         w1 = w1 - STEP * w1Slope;
         console.log(" w0Slope = " + w0Slope + ", w1Slope = " + w1Slope);
 
+        // Cost Method 1: difference in RSS
         prevRSS = rss;
-        rss = costFunction(w0, w1);
+        rss = computeRSS(w0, w1);
+        // cost = rss - prevRSS;
+
+        // Cost Method 2: euclidean length
+        cost = euclideanLength(w0Slope, w1Slope) ;
+
         console.log("new RSS = " + rss);
-    } while (Math.abs(rss - prevRSS) > tolerance);
+    } while (Math.abs(cost) > tolerance);
 
     console.log("\n\nSOLUTION: w0 = " + w0 + ", w1 = " + w1 + "  in " + iterations + " iterations\n\n");
     return {w0: w0, w1: w1, iterations: iterations};
